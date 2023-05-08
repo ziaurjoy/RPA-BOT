9b2867aee171fca1fd3894d413b5ed7d4a41ce5d
ziaurjoy <ziaurjoy802@gmail.com>
2023-05-08 15:34:31 +0600

    readme file database

---
+++ README.md
@@ -0,0 +1,17 @@
+# Requirements
+
+- Python
+- Mysql
+- Google Chrome Browser
+
+# Step1
+
+- Create Virtualenv
+- Virtualenv Activate
+- pip install -r requirements.txt
+
+# Step2
+
+- Update Google Chrome Browser
+- Mysql DB server running
+- python rpa_bot.py
--- rpa.py
+++ rpa.py
@@ -5,6 +5,38 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options  # for suppressing the browser
 
+import mysql.connector
+
+# first time error throw becouse db not created.So error handle
+# if db all ready created so not throw error
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="amazonscriptingdb"
+    )
+except:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+    )
+
+mycursor = mydb.cursor()
+
+try:
+    mycursor.execute("CREATE DATABASE amazonscriptingdb")
+except:
+    pass
+
+try:
+	mycursor.execute("CREATE TABLE product_collects(Id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(255), price VARCHAR(225), shipment VARCHAR(225), feature TEXT, image_url VARCHAR(225), update_status BOOLEAN DEFAULT false)")
+except:
+    pass
+
+product_collects_sql = "INSERT INTO product_collects (title, price, shipment, feature, image_url, update_status) VALUES (%s, %s, %s, %s, %s, %s)"
+
 
 class RPA:
 	# this code useing without open browser
@@ -26,8 +58,11 @@ class RPA:
 	driver.get('https://www.amazon.com/')
 	time.sleep(3)
 
-	for i in range(2):
+	for i in range(5):
 		search_key = input('Plese Input The Search key for Amazon : ')
+
+
+		# this method working for searching key query result items and return all item links
 		def search_item_product_link(driver, search_key):
 			search_input_element = driver.find_element(By.ID, 'twotabsearchtextbox')
 			search_input_element.clear()
@@ -44,15 +79,92 @@ class RPA:
 			for item in search_items:
 				try:
 					driver.implicitly_wait(1)
-					link = item.find_element(
-						By.TAG_NAME, 'a').get_attribute("href")
+					link = item.find_element(By.TAG_NAME, 'a').get_attribute("href")
 					search_item_list.append(link)
 				except NoSuchElementException:
 					continue
 			return search_item_list
+		
 		links = search_item_product_link(driver=driver, search_key=search_key)
-		print('==================================================')
-		print(links)
-		print('==================================================')
+
+
+		# this method view to product details page and collect data and save to database
+		def product_information_save_db(driver, link):
+			try:
+				driver.set_page_load_timeout(300)
+				driver.get(link)
+			except:
+				return True
+			try:
+				driver.implicitly_wait(1)
+				title = driver.find_element(By.ID, "productTitle").text
+			except NoSuchElementException:
+				title = ''
+			try:
+				driver.implicitly_wait(1)
+				price = driver.find_element(By.CLASS_NAME, "reinventPricePriceToPayMargin").text
+			except NoSuchElementException:
+				price = ''
+			try:
+				driver.implicitly_wait(1)
+				shipment = driver.find_element(By.ID, "deliveryBlockContainer").text
+			except NoSuchElementException:
+				shipment = ''
+
+			try:
+				driver.implicitly_wait(1)
+				feature = driver.find_element(By.ID, "featurebullets_feature_div").text
+			except NoSuchElementException:
+				feature = ''
+
+			try:
+				driver.implicitly_wait(1)
+				image_div_element = driver.find_element(By.ID, "imgTagWrapperId")
+				image_url = image_div_element.find_element(By.TAG_NAME, "img").get_attribute("src")
+			except NoSuchElementException:
+				image_url = ''
+			if title != '':
+				value_product_details = (title, price, shipment, feature, image_url, False)
+				# execute sql query
+				mycursor.execute(product_collects_sql, value_product_details)
+				mydb.commit()  # push data for database table
+				return int(mycursor.lastrowid)
+			return True
+		
+		
+		# this method work to query db to last save data in database and update value 
+		def query_db_and_update(id):
+			try:
+				sql = f"SELECT * FROM product_collects WHERE id ={id}"
+				mycursor.execute(sql)
+				get_data = mycursor.fetchone()
+
+				title = get_data[1]
+				price = get_data[2]
+				shipment = get_data[3]
+				feature = get_data[4]
+				image_url = get_data[5]
+				update_status = True
+
+				query_sql = "Update product_collects SET title=%s, price=%s, shipment=%s, feature=%s, image_url=%s, update_status=%s WHERE id=%s"
+				mycursor.execute(query_sql, (title, price, shipment, feature, image_url, update_status, id))
+				mydb.commit()
+			except:
+				pass
+
+
+		for link in links:
+			if type(link) == str: # some time link_list in None so type error becouse statwith not support None value
+				if link.startswith('https://www.amazon.com/')==True and link.startswith('https://www.amazon.com/gp/help/')==False and link.startswith('https://www.amazon.com/s?k=')==False: # Only work item product details link
+					id = product_information_save_db(driver, link) # this method save data in database and return this id or error return true
+					if type(id) == int:
+						query_db_and_update(id) # query item and update 
+					else:
+						continue
+				else:
+					continue
+			else:
+				continue
+
 			
-	input('break')
+	print("All Data Save In DataBase Please Check Database")
